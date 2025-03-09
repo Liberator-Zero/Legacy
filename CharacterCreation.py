8b9503c5ac2d39f4cfb20b3053a8a15174f41045
Christopher Little <christopher.s.little@protonmail.com>
2025-03-08 21:19:32 -0500

    Create CharacterCreation.py
    Initial Commit of "Legacy"

---
+++ CharacterCreation.py
@@ -0,0 +1,29 @@
+character_name = input("Tell us your name Adventurer: ").strip()
+
+print(f"\nWelcome to Legacy, {character_name}!")
+
+def character_race_menu():
+    menu = r"""
+        ┌────────────────────────────────────┐
+        │          CHOOSE YOUR RACE          │
+        ├────────────────────────────────────┤
+        │ (+) Monk                           │
+        │ (+) Paladin                        │
+        │ (+) Sorcerer                       │
+        │ (+) Blood Elf                      │
+        │ (+) Human Barbarian                │
+        └────────────────────────────────────┘
+        """
+    print(menu)
+
+    valid_races = {"Monk", "Paladin", "Sorcerer", "Blood Elf", "Human Barbarian", "???" }
+    while True:
+        race = input("Choose your race: ").strip().title()
+
+        if race in valid_races:
+            return race
+        else:
+            print("Please check your spelling and try again!")
+
+character_race = character_race_menu()
+print(f"\n{character_name} the almighty {character_race}.")
